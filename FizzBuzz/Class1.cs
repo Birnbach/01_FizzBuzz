41d1536890a0e876a480d9fc712766f24a0d8eec
PlatformyBirPreis <242050@student.pwr.edu.pl>
2020-03-25 21:47:38 +0100

    lab_01

---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List<string> results = new List<string>;
+            Console.WriteLine("Hello World!");
+            List<int> numbers = new List<int> { 1, 4532, 51, 31, 11531, 22354 };
+
+            var results = FizzBuzzer.MillNumbers(numbers);
+            results.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public static List<string> MillNumbers(List<int> numbers)
+        {
+            string intoString;
+            List<string> result = new List<string>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 7 != 0)
+                {
+                    intoString = numbers[i].ToString();
+                    if (numbers[i] % 3 == 0 && numbers[i] % 5 == 0 || intoString.Contains("53") || intoString.Contains("35"))
+                    {
+                        result.Add("FizzBuzz");
+                    }
+                    else if (numbers[i] % 3 == 0)
+                    {
+                        result.Add("Fizz");
+                    }
+                    else if (numbers[i] % 5 == 0 || intoString.Contains("5"))
+                    {
+                        result.Add("Buzz");
+                    }
+                    else
+                        result.Add(numbers[i].ToString());
+                }
+
+                else
+                {
+                    result.Add("Buzzinga");
+                }
+            }
+            return result;
+        }
+    }
+}
